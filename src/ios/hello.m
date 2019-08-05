af53057834b7306b69675562f495ab8cc6185cc8
Riyaz Mohammed <riyaz.mohammed@zohocorp.com>
2019-08-05 21:07:23 +0530

    initial commit

---
+++ src/ios/hello.m
@@ -0,0 +1,42 @@
+/********* hello.m Cordova Plugin Implementation *******/
+
+#import <Cordova/CDV.h>
+
+@interface hello : CDVPlugin {
+  // Member variables go here.
+}
+
+- (void)coolMethod:(CDVInvokedUrlCommand*)command;
+@end
+
+@implementation hello
+
+- (void)coolMethod:(CDVInvokedUrlCommand*)command
+{
+    CDVPluginResult* pluginResult = nil;
+    NSString* echo = [command.arguments objectAtIndex:0];
+
+    if (echo != nil && [echo length] > 0) {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
+    } else {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+    }
+
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+- (void)add:(CDVInvokedUrlCommand*)command
+
+    CDVPluginResult* pluginResult = nil;
+    NSString* echo1 = [command.arguments objectAtIndex:0];
+    NSString* echo2 = [command.arguments objectAtIndex:1];
+
+    if (echo != nil && [echo length] > 0) {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
+    } else {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+    }
+    
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+@end
