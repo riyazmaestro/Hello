01190872f193f6f536c4b3ca72ee5df8a7c8e430
Riyaz Mohammed <riyaz.mohammed@zohocorp.com>
2019-08-05 21:29:49 +0530

    error fic

--- src/ios/hello.m
+++ src/ios/hello.m
@@ -26,17 +26,21 @@
 }
 
 - (void)add:(CDVInvokedUrlCommand*)command
-
-    CDVPluginResult* pluginResult = nil;
+{
+    CDVPluginResult * pluginResult = nil;
     NSString* echo1 = [command.arguments objectAtIndex:0];
     NSString* echo2 = [command.arguments objectAtIndex:1];
-
-    if (echo != nil && [echo length] > 0) {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
+    
+    NSLog(echo1);
+    NSLog(echo2);
+    
+    if (echo1 != nil && [echo1 length] > 0) {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo1];
     } else {
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
     }
     
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
+
 @end
